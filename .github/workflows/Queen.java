a4500a6753b0f7ea07590d4f57706379b74f8713
rishis123 <73186616+rishis123@users.noreply.github.com>
2023-05-12 09:05:43 -0400

    Update Queen.java

--- .github/workflows/Queen.java
+++ .github/workflows/Queen.java
@@ -1,7 +1,11 @@
 package Game;
 
 public class Queen extends Piece {
+	private 
 	public Queen(boolean c) {
 		super("queen", c, 9);
 	}
+	
+	public void allPossibleMoves() { // inherit from abstract superclass ??
+		
 }
