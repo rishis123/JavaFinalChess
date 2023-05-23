956f03cd4c557c81611839282ebed33cd8a04d8b
nicolasshul <nicolasshulga@hunterschools.org>
2023-05-23 00:29:16 -0400

    fixed constructor

--- .github/workflows/Bishop.java
+++ .github/workflows/Bishop.java
@@ -1,6 +1,6 @@
 public class Bishop extends Piece {
-	public Bishop(boolean c) {
-		super("bishop", c, 3);
+	public Bishop(boolean c, int col, int row) {
+		super("bishop", c, col, row, 3);
 	}
 
 	public boolean isLegal(int toRow, int toCol, Board _b) {
