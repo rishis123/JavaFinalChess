3d4f534aa375757e994332a4e63e763dcfccb200
rishis123 <73186616+rishis123@users.noreply.github.com>
2023-05-24 09:20:20 -0400

    Update Knight.java

--- .github/workflows/Knight.java
+++ .github/workflows/Knight.java
@@ -1,9 +1,25 @@
 public class Knight extends Piece {
+	private int currRow; 
+	private int currCol;
+	
 	public Knight(boolean c, int row, int col) {
 		super("knight", c, row, col, 3);
+		currRow = row;
+		currCol = col;
 	}
 
 	public boolean isLegal(int toRow, int toCol, Board _b) {
+		
+		
+	//don't need to worry about pieces in the middle
+		int diffRow = Math.abs(currRow - toRow);
+		int diffCol = Math.abs(currCol - toCol);
+		if ((diffRow != 0 && diffCol == 0) && (diffRow + diffCol == 3)) { // valid move combo
+			
+			
+			
+		if (
+		
 	    return false; // implement later
 	}
 }
