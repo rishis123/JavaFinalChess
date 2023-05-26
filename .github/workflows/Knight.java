3ecb334852dd469d886d373904ed7dc1f138bc4b
rishis123 <73186616+rishis123@users.noreply.github.com>
2023-05-25 23:43:42 -0400

    Update Knight.java

--- .github/workflows/Knight.java
+++ .github/workflows/Knight.java
@@ -9,17 +9,144 @@ public class Knight extends Piece {
 		currCol = col;
 	}
 	
-	public int[][] getPossibleMoves(Board b) {
-		Pieces[][] board = b.getBoard();
-		int[][] possibleMoves = new int[2][8]; //rishi i started it for you, just fill this array w/ all the possible moves. row 0 is the row, row 1 is the column.
-		for(int i = 0; i < possibleMoves[0].length; i++) {
+	public int[][] getPossibleMoves(Board b) { // fully done for knight
+		Piece[][] board = b.getBoard();
+		int[][] possibleMoves = new int[2][8]; // 8 is max possible moves for a knight
+		for (int i = 0; i < possibleMoves[0].length; i++) {
 			possibleMoves[0][i] = -1;
 			possibleMoves[1][i] = -1;
-		}			
+ 		}
 		
-		// include all the 2s, then 1s, don't need 1s and 2s again since double counting
+		int row = getRow();
+		int col = getColumn();
+		int i = 0;
 		
-		//
+		//Let's start with the two up, one left, using if functionality since don't need while to go through
+		row = row + 2;
+		col = col-1;
+		if (row <= 7 && col >= 0 && board[row][col] == null) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		if (row <= 7 && col>= 0 && board[row][col].getColor()! = getColor()) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		//now two up, one right
+		
+		row = getRow() + 2;
+		col = getColumn() + 1;
+		if (row <= 7 && col <= 7 && board[row][col] == null) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		if (row <= 7 && col <= 7 && board[row][col].getColor()! = getColor()) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		//now one up, two left
+		row = getRow() + 1;
+		col = getColumn() -2;
+		if (row <= 7 && col >=0 && board[row][col] == null) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		if (row <= 7 && col >=0 && board[row][col].getColor()! = getColor()) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		//now one up, two right
+		row = getRow() + 1;
+		col = getColumn() +2;
+		if (row <= 7 && col <=7 && board[row][col] == null) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		if (row <= 7 && col <=7 && board[row][col].getColor()! = getColor()) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		//now one down, two left
+		row = getRow() -1;
+		col = getColumn() -2;
+		if (row >= 0 && col >=0 && board[row][col] == null) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		if (row >= 0 && col >=0 && board[row][col].getColor()! = getColor()) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		// now one down, two right
+		
+		row = getRow() -1;
+		col = getColumn() +2;
+		if (row >= 0 && col <=7 && board[row][col] == null) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		if (row >= 0 && col <=7 && board[row][col].getColor()! = getColor()) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		//now two down, one left
+		row = getRow() -2;
+		col = getColumn() -1;
+		if (row >= 0 && col >=0 && board[row][col] == null) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		if (row >= 0 && col >=0 && board[row][col].getColor()! = getColor()) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		//now two down, one right
+		
+		row = getRow() -2;
+		col = getColumn() +1;
+		if (row >= 0 && col <=7 && board[row][col] == null) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		if (row >= 0 && col <= 7 && board[row][col].getColor()! = getColor()) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+	
+		return possibleMoves;	
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
