5a8c1cbd2ff1d57af3f984f3c95f2de2fbe16c53
merlinothefirst <34113706+merlinothefirst@users.noreply.github.com>
2023-05-30 18:55:48 -0400

    Update Board.java

--- .github/workflows/Board.java
+++ .github/workflows/Board.java
@@ -1,31 +1,33 @@
+import java.util.ArrayList;
+
 public class Board {
 	private Piece[][] board; //rows are letters, columns are numbers; top left is a8, bottom right is h1
 	
 	public Board() {
 		board = new Piece[8][8];
 		for(int i = 0; i < 8; i++) {
-			board[7][i] = new Pawn(true, 6, i);
-			board[1][i] = new Pawn(false, 1, i);
+			board[i][6] = new Pawn(true, i, 6);
+			board[i][1] = new Pawn(false, i, 1);
 			
 			if(i == 0 || i == 7) {
-				board[7][i] = new Rook(true, 7, i);
-				board[0][i] = new Rook(false, 0, i);
+				board[i][7] = new Rook(true, i, 7);
+				board[i][0] = new Rook(false, i, 0);
 			}
 			if(i == 1 || i == 6) {
-				board[7][i] = new Knight(true, 7, i);
-				board[0][i] = new Knight(false, 0, i);
+				board[i][7] = new Knight(true, i, 7);
+				board[i][0] = new Knight(false, i, 0);
 			}
 			if(i == 2 || i == 5) {
-				board[7][i] = new Bishop(true, 7, i);
-				board[0][i] = new Bishop(false, 0, i);
+				board[i][7] = new Bishop(true, i, 7);
+				board[i][0] = new Bishop(false, i, 0);
 			}
 			if(i == 3) {
-				board[7][i] = new Queen(true, 7, i);
-				board[0][i] = new Queen(false, 0, i);
+				board[i][7] = new Queen(true, i, 7);
+				board[i][0] = new Queen(false, i, 0);
 			}
 			if(i == 4) {
-				board[7][i] = new King(true, 7, i);
-				board[0][i] = new King(false, 0, i);
+				board[i][7] = new King(true, i, 7);
+				board[i][0] = new King(false, i, 0);
 			}
 		}
 	}
@@ -34,25 +36,9 @@ public class Board {
 		return board;
 	}
 	
-	/**
-	Sets a position on 2D array to an inputted piece -- useful in the swaps for capturing, castling, and pawn promotion. 
-	@param p the piece that you want to replace existing piece with
-	@param row the row value of the replacing piece (note: 0-based indexing)
-	@param col the column value of the replacing piece (note: again 0-based indexing
-	
-	*/
-	
 	public void setSquare(Piece p, int row, int col) {
 		board[row][col] = p;
 	}
-	
-	/**
-	Returns the piece at a specified square / position in the 2D array
-	
-	@param r the row value of the specific piece, as seen in 2D array
-	@param c the column value of the same piece.
-	
-	*/
 	
 	public Piece getSquare(int r, int c) {
 		return board[r][c];
