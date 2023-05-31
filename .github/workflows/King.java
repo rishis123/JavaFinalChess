64a502848a505930d18b7c2935ecdfab2d90619c
rishis123 <73186616+rishis123@users.noreply.github.com>
2023-05-31 19:37:02 -0400

    Update King.java

--- .github/workflows/King.java
+++ .github/workflows/King.java
@@ -8,6 +8,78 @@ public class King extends Piece {
 	}
 
 	public int[][] getPossibleMoves(Board b) {
-		return null; //implement later
+		Piece[][] board = b.getBoard();
+		int[][] possibleMoves = new int[2][8]; // 8 is max possible moves for a king
+		for (int i = 0; i < possibleMoves[0].length; i++) {
+			possibleMoves[0][i] = -1;
+			possibleMoves[1][i] = -1;
+ 		}
+		
+		int row = getRow();
+		int col = getCol();
+		int i = 0;
+		
+		row = getRow() -1 ;
+		if (row >= 0 && (board[row][col] == null || (board[row][col].getColor() ! = getColor()))) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		row = getRow() + 1;
+		if (row <= 7 && (board[row][col] == null || (board[row][col].getColor() ! = getColor()))) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		row = getRow() -1;
+		col = getCol() - 1;
+		if (row >= 0 && col >= 0 && (board[row][col] == null || (board[row][col].getColor() ! = getColor()))) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		row = getRow() - 1;
+		col = getCol() +1;
+		if (row >= 0 && col <= 7 && (board[row][col] == null || (board[row][col].getColor() ! = getColor()))) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		row = getRow() + 1;
+		col = getCol() -1;
+		if (row <= 7 && col >=0 && (board[row][col] == null || (board[row][col].getColor() ! = getColor()))) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		row = getRow() + 1;
+		col = getCol() + 1;
+		if (row <= 7 && col <=7 && (board[row][col] == null || (board[row][col].getColor() ! = getColor()))) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		row = getRow();
+		col = getCol() + 1;
+		if (col <= 7 && (board[row][col] == null || (board[row][col].getColor() ! = getColor()))) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+		
+		col = getCol() -1;
+		if (col >= 0 && (board[row][col] == null || (board[row][col].getColor() ! = getColor()))) {
+			possibleMoves[0][i] = row;
+			possibleMoves[1][i] = col;
+			i++;
+		}
+
+		return possibleMoves;
+		
 	}
 }
