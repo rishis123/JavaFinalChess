29240aa2c456e823e411df03a5c4a7239450fbb4
nicolasshul <nicolasshulga@hunterschools.org>
2023-05-31 20:06:38 -0400

    it doenst work

--- .github/workflows/King.java
+++ .github/workflows/King.java
@@ -7,7 +7,18 @@ public class King extends Piece {
 	    return false; // implement later
 	}
 
-	public int[][] getPossibleMoves(Board b) {
-		return null; //implement later
+	public int[][] getPossibleMoves(Board _b) {
+		Piece[][] board = _b.getBoard();
+		int[][] possibleMoves = new int[2][8];
+		int j = 0;
+		for (int i = 0; i < possibleMoves[0].length; i++) {
+			possibleMoves[0][i] = -1;
+			possibleMoves[1][i] = -1;
+		}
+		
+		int row = getRow();
+		int col = getCol();
+		
+		return possibleMoves;
 	}
 }
